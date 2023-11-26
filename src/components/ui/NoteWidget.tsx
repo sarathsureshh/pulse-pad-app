e63af6873a30532886fa642ba5881b72d16bcde5
Sarath Suresh <sarathsuresh9360@gmail.com>
2023-11-26 19:34:23 +0530

    NotesWidget populated

--- src/components/ui/NoteWidget.tsx
+++ src/components/ui/NoteWidget.tsx
@@ -1,6 +1,19 @@
 import React from "react";
-const NoteWidget = () => {
-  return <>Note Widget</>;
+
+interface NoteWidgetProps {
+  heading: string;
+  body: string;
+}
+
+const NoteWidget: React.FC<NoteWidgetProps> = ({ heading, body }) => {
+  return (
+    <>
+      <div className="bg-white-100 p-5 rounded-lg shadow-md max-w-md m-2 border-black">
+        <h2 className="text-lg font-semibold mb-2">{heading}</h2>
+        <p className="text-gray-700">{body}</p>
+      </div>
+    </>
+  );
 };
 
 export default NoteWidget;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,54 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <NoteWidget />
+      <div className="flex flex-wrap justify-center">
+        {notes.map((note) => {
+          return <NoteWidget heading={note.heading} body={note.body} />;
+        })}
+      </div>
     </>
   );
 }
+
+const notes = [
+  {
+    heading: "First Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Second Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Third Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+  {
+    heading: "Fourth Notes",
+    body: "This is the content of your note. You can add your text here.",
+  },
+];
